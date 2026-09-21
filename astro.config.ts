a31153e5d2c067d2db880b02bb4ac4f9bae6a964
Dan Humphreys <dan.humphreys@xde.dev>
2026-09-22 00:03:42 +0100

    Update favicon and add logo.

--- astro.config.ts
+++ astro.config.ts
@@ -12,6 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   output: 'static',
   site: 'https://ferensway.com',
+  image: { dangerouslyProcessSVG: true },
   integrations: [sitemap(), astrowind({ config: './src/config.yaml' })],
   vite: {
     plugins: [tailwindcss()],
